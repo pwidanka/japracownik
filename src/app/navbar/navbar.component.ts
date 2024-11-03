e915dc3aa9ae6cd309f1620094f51cae3302cc27
Przemek <przemyslaw.widanka@gmail.com>
2024-11-03 22:08:31 +0100

    update wersji

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterLinkActive],
+  imports: [FlexLayoutModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterLinkActive, MatSidenavModule, MatListModule, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
